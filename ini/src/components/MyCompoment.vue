e9da0e949224c522c7d148825355520ff07fb434
Razyapova Aliya <a.raziapova@mail.ru>
2023-03-30 22:32:45 +0300

    [add] MyCompoment

---
+++ ini/src/components/MyCompoment.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="root">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyCompoment",
+  data() {
+    return {
+      text: "Hello",
+    };
+  },
+};
+</script>
