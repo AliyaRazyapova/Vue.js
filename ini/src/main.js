b627f12e53d9f231c6c4cb42a07708a3903f95e2
Razyapova Aliya <a.raziapova@mail.ru>
2023-03-31 16:24:19 +0300

    [edit] plugins

--- ini/src/main.js
+++ ini/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import simplePlugin from './plugins/simplePlugin'
+
+import '@/assets/styles/main.scss';
 
 Vue.config.productionTip = false
+Vue.use(simplePlugin)
+
 
 new Vue({
   render: h => h(App),
